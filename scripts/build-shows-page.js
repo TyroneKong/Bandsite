e36f117ee86634fc63a0d4c2c150e37f37059856
Tyrone Kong <tyronekong@gmail.com>
2022-02-11 00:53:07 +0000

    applied sizing to comment section and shows columns

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,82 +1,75 @@
+// shows arrray
 const shows = [
-    {
-      DATE: "Mon Sept 06 2021",
-      VENUE: "Ronald Lane",
-      LOCATION: "San Francisco, CA",
-    },
-    {
-      DATE: "Tue Sept 21 2021",
-      VENUE: "Pier 3 East",
-      LOCATION: "San Francisco, CA",
-    },
-    {
-      DATE: "Fri Oct 15 2021",
-      VENUE: "View Lounge",
-      LOCATION: "San Francisco, CA",
-    },
-    {
-      DATE: "Sat Nov 06 2021",
-      VENUE: "Hyatt Agency",
-      LOCATION: "San Francisco, CA",
-    },
-    {
-      DATE: "Fri Nov 26 2021",
-      VENUE: "Moscow Center",
-      LOCATION: "San Francisco, CA",
-    },
-    {
-      DATE: "Wed Dec 15 2021",
-      VENUE: "Press Club",
-      LOCATION: "San Francisco, CA",
-    },
-  ];
-  
-  
- 
-  const showInfo = document.querySelector('.show__list')
-  
-  // loop
-  for (let i = 0; i < shows.length; i++) {
-  
+  {
+    DATE: "Mon Sept 06 2021",
+    VENUE: "Ronald Lane",
+    LOCATION: "San Francisco, CA",
+  },
+  {
+    DATE: "Tue Sept 21 2021",
+    VENUE: "Pier 3 East",
+    LOCATION: "San Francisco, CA",
+  },
+  {
+    DATE: "Fri Oct 15 2021",
+    VENUE: "View Lounge",
+    LOCATION: "San Francisco, CA",
+  },
+  {
+    DATE: "Sat Nov 06 2021",
+    VENUE: "Hyatt Agency",
+    LOCATION: "San Francisco, CA",
+  },
+  {
+    DATE: "Fri Nov 26 2021",
+    VENUE: "Moscow Center",
+    LOCATION: "San Francisco, CA",
+  },
+  {
+    DATE: "Wed Dec 15 2021",
+    VENUE: "Press Club",
+    LOCATION: "San Francisco, CA",
+  },
+];
 
-    const listDate = document.createElement('li')
-    const listVenue = document.createElement('li')
-    const listLocation = document.createElement('li')
-    const titleDate = document.createElement('h3')
-    const titleVenue = document.createElement('h3')
-    const titleLocation = document.createElement('h3')
-    const button = document.createElement("button");
-    const divider = document.createElement('hr')
+// target dom element
+const showInfo = document.querySelector(".show__list");
 
-    listDate.classList.add('show__date')
-    listVenue.classList.add('.show__venue')
-    listLocation.classList.add('.show__location')
-    button.classList.add('buy')
-    divider.classList.add('show__divider')
-    showInfo.appendChild(titleDate)
-    showInfo.appendChild(listDate)
-    showInfo.appendChild(titleVenue)
-    showInfo.appendChild(listVenue)
-    showInfo.appendChild(titleLocation)
-    showInfo.appendChild(listLocation)
-    showInfo.appendChild(button)
-    showInfo.appendChild(divider)
-    listDate.innerText = shows[i].DATE
-    listVenue.innerText = shows[i].VENUE
-    listLocation.innerText = shows[i].LOCATION
-    titleDate.innerText = 'DATE'
-    titleVenue.innerText = 'VENUE'
-    titleLocation.innerText = 'LOCATION'
-    button.innerText = 'BUY TICKETS'
-    
-    
+// display shows
+for (let i = 0; i < shows.length; i++) {
+  // create new elements
+  const listDate = document.createElement("li");
+  const listVenue = document.createElement("li");
+  const listLocation = document.createElement("li");
+  const titleDate = document.createElement("h3");
+  const titleVenue = document.createElement("h3");
+  const titleLocation = document.createElement("h3");
+  const button = document.createElement("button");
+  const divider = document.createElement("hr");
 
-   
-  }
-  
-  
+  // add class names
+  listDate.classList.add("show__date");
+  listVenue.classList.add(".show__venue");
+  listLocation.classList.add(".show__location");
+  button.classList.add("buy");
+  divider.classList.add("show__divider");
 
-  
-  
+  // append children to parent
+  showInfo.appendChild(titleDate);
+  showInfo.appendChild(listDate);
+  showInfo.appendChild(titleVenue);
+  showInfo.appendChild(listVenue);
+  showInfo.appendChild(titleLocation);
+  showInfo.appendChild(listLocation);
+  showInfo.appendChild(button);
+  showInfo.appendChild(divider);
 
-  
+  // add inner text
+  listDate.innerText = shows[i].DATE;
+  listVenue.innerText = shows[i].VENUE;
+  listLocation.innerText = shows[i].LOCATION;
+  titleDate.innerText = "DATE";
+  titleVenue.innerText = "VENUE";
+  titleLocation.innerText = "LOCATION";
+  button.innerText = "BUY TICKETS";
+}
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,87 +1,92 @@
-  
-  // comments array 
-  const comments= [{
-    Name:'Mile Acosta',
-    Date:"12/20/2020",
-    Comment:"I can stop listening Every time I hear once of their songs-the vocals - it gives me goosebumps.Shivers straight down my spine. What a beautiful expression of creativity. Cant get enough"
-    },
-    {
-    Name: 'Emilie Beach',
-    Date:"01/09/2021",
-     Comment:"I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
+// comments array
+const comments = [
+  {
+    Name: "Mile Acosta",
+    Date: "12/20/2020",
+    Comment:
+      "I can stop listening Every time I hear once of their songs-the vocals - it gives me goosebumps.Shivers straight down my spine. What a beautiful expression of creativity. Cant get enough",
+  },
+  {
+    Name: "Emilie Beach",
+    Date: "01/09/2021",
+    Comment:
+      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+  },
+  {
+    Name: "Connor Walton",
+    Date: "02/17/2021",
+    Comment:
+      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+  },
+];
 
-    },
-    {
-    Name: 'Connor Walton',
-    Date:"02/17/2021",
-    Comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
-    }
+// target dom elements
 
-    ]
+const commentContainer = document.querySelector(".comment__list");
+const commentBtn = document.querySelector(".comment-btn");
 
-
-
-const commentContainer = document.querySelector('.comment__list')
-const commentBtn = document.querySelector('.comment-btn')
-
-
+// current date
+const currentDate = new Date("10-Feburary-2022");
+const formattedDate = currentDate.toLocaleDateString("en-GB", {
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+});
 
 //apply new comment
 
-const applyNewComment=(event)=>{
+const applyNewComment = (event) => {
+  const nameInput = document.querySelector(".input").value;
+  const commentInput = document.querySelector(".text").value;
+  const commentList = document.querySelector(".comment__list");
+  comments.push({
+    Name: nameInput,
+    Date: formattedDate,
+    Comment: commentInput,
+  });
+  console.log(comments);
 
-const nameInput = document.querySelector('.input').value
-const commentInput = document.querySelector('.text').value
-const commentList = document.querySelector('.comment__list')
-comments.push({Name:nameInput, Comment:commentInput})
-console.log(comments)
-
-commentList.innerText = ""
-event.preventDefault()
- displayComment()
-
-
-}
-
+  commentList.innerText = "";
+  event.preventDefault();
+  displayComment();
+};
 
 //button event
 
-commentBtn.addEventListener('click', (applyNewComment))
+commentBtn.addEventListener("click", applyNewComment);
 
-   
+// display default comments
+const displayComment = () => {
+  for (let i = comments.length - 1; i >= 0; i--) {
+    // target dom elements
+    const commentList = document.querySelector(".comment__list");
 
+    // create new elements
+    const date = document.createElement("p");
+    const nameTitle = document.createElement("h3");
+    const commenting = document.createElement("li");
+    const avatar = document.createElement("img");
+    const divider = document.createElement("hr");
 
-// display default comments 
-const displayComment=()=>{
-    for(let i = comments.length-1;i>=0; i--){
-        const commentList = document.querySelector('.comment__list')
-        const heading = document.querySelector('.heading')
+    // add classes to new elements
+    date.classList.add("comment__date");
+    avatar.classList.add("avatar__img");
+    nameTitle.classList.add("comment__name");
+    commenting.classList.add("comment__item");
+    divider.classList.add("divider");
 
-        const date = document.createElement('p')
-        const nameTitle = document.createElement('h3')
-        const commenting = document.createElement('li')
-        const avatar = document.createElement('img')
-        const divider = document.createElement('hr')
+    // append children to parent element
+    commentList.appendChild(avatar);
+    commentList.appendChild(date);
+    commentList.appendChild(nameTitle);
+    commentList.appendChild(commenting);
+    commentList.appendChild(divider);
 
-        date.classList.add('comment__date')
-        avatar.classList.add('avatar__img')
-        nameTitle.classList.add('comment__name')
-        commenting.classList.add('comment__item')
-        commentList.appendChild(avatar)
-        commentList.appendChild(date)
-        commentList.appendChild(nameTitle)
-       
-     
-        commentList.appendChild(commenting)
-        commentList.appendChild(divider)
-        nameTitle.innerText = comments[i].Name
-        date.innerText = comments[i].Date    
-        commenting.innerText = comments[i].Comment
+    // add content
+    nameTitle.innerText = comments[i].Name;
+    date.innerText = comments[i].Date;
+    commenting.innerText = comments[i].Comment;
+  }
+};
 
-    }
-}
-
-displayComment()
-
-
-
+displayComment();
